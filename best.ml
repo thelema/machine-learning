c22f3e4584cc02a0b6bf74ef1d4949dadd2ff931
Eric Norige <thelema314@gmail.com>
2011-04-27 11:52:49 -0400

    Do predictions in chunks

--- best.ml
+++ best.ml
@@ -3,8 +3,9 @@ open Printf
 open Predict
 open Bigarray
 
-let test_data = Datafile.get_matrix "testing.txt.ba"
 
+let test_data = Datafile.get_matrix "testing.txt.ba"
+let batch_size = 10000
 
 let () = 
   let make_best infile = 
@@ -12,8 +13,16 @@ let () =
     let predictor = pred_read infile in
     printf "Predicting%!";
     let t0 = Sys.time() in
-    let preds = predict_many (predict_cat predictor) test_data |> List.enum in
-    File.with_file_out outall (fun oc -> Enum.iter (print_pred_conf oc) preds);
+    let n = Array2.dim2 test_data in
+    let batches = n / batch_size in
+    let oc = File.open_out outall in
+    for i = 0 to batches do (* extra loop at the end for any leftover *)
+      let i0 = i * batch_size in
+      let slice = Array2.sub_right test_data (i0+1) (i0+batch_size) in
+      batch_predict_cat predictor slice |> Array.iter (print_pred_conf oc);
+      printf ",";
+    done;
+    IO.close_out oc;
     printf "Done (%.2fs)\n%!" (Sys.time () -. t0);
   in
   args () |> iter make_best
